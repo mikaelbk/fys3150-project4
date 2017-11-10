65c2f686040cfded84e0dccba1d8959cacaccda0
Mikael B. Kiste <mikael.kiste@gmail.com>
2017-11-10 20:16:03 +0100

    input arguments working (damn its late and im not sober but this is fun)

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <armadillo>
 #include <random>
+#include <math.h>
 using namespace std;
 using namespace arma;
 
@@ -16,15 +17,25 @@ int main(int argc, char const *argv[])
 	if (argc != 7) {
 		cout << "\tError: Invalid number of arguments. Program requires the following call:" << endl;
 		cout << "\t " << argv[0] << " outFileName L mcc ti tf dt" << endl;
-		cout << "\tWhere the arguments are:\n\toutFileName:\tName that will be given to the output file\n\tL:\tSquare root of the number of lattice points for the LxL lattice\n\tmcc:\tTotal number of Montecarlo cycles. Each cycle performes L^2 number of Metropolis algorithm cycles\n\tti:\tstarting temperature of experiment\n\ttf:\tfinal temperature of experiment\n\tdt:\ttemperature step between measurements" << endl;
+		cout << "\tWhere the arguments are:\n\toutFileName:\tName that will be given to the output file\n\tL:\tSquare root of the number of lattice points for the LxL lattice\n\tmcc:\tNumber of Montecarlo cycles. If less than 20 then is set to 10^mcc\n\tti:\tstarting temperature of experiment\n\ttf:\tfinal temperature of experiment\n\tdt:\ttemperature step between measurements" << endl;
 		exit(1);
 	}
 
 	// declaration of input arguments
 	string outFileName = argv[1];
+	int L = atoi(argv[2]);
+	int mcc = atoi(argv[3]);
+	if(mcc < 20){mcc = int(pow(10,mcc));};
+	double ti = atof(argv[4]);
+	double tf = atof(argv[5]);
+	double dt = atof(argv[6]);
 
-	int L = 20; // lattice size is L²
-	double T = 1.0;
+	cout << outFileName << endl;
+	cout << L << endl;
+	cout << mcc << endl;
+	cout << ti << endl;
+	cout << tf << endl;
+	cout << dt << endl;
 
 	// Initialize the seed and call the Mersienne algo
 	std::random_device rd;
