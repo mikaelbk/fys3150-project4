a3be0501d76835de49410983388ee281d6b176ca
Mikael B. Kiste <mikael.kiste@gmail.com>
2017-11-18 22:37:13 +0100

    fuck my life...

--- program/compareResults.py
+++ program/compareResults.py
@@ -4,27 +4,38 @@ from readtxt import *
 
 t1a = get_data("20x20_t1.000000_aligned")
 t1r = get_data("20x20_t1.000000_random")
-t2a = get_data("20x20_t2.700000_aligned")
-t2r = get_data("20x20_t2.700000_random")
+t2a = get_data("20x20_t2.400000_aligned")
+t2r = get_data("20x20_t2.400000_random")
+"""
+figure(1)
+subplots_adjust(bottom=-0.2)
+subplot(4,1,1)
+plot((log10(t1a["mcc"])),t1a["E"])
+title('T=1, ordered initial lattice')
+axis([0,5,-2.0,-1.996])
+#subplot(2,2,2)
+#plot(log10(((t1r["mcc"]))),(t1r["E"]))
+subplot(4,1,2)
+plot(log10(((t2a["mcc"]))),(t2a["E"]))
+title('T=2.4, ordered initial lattice')
+subplot(4,1,3)
+plot(log10(((t1r["mcc"]))),(t1r["E"]), label = 'T=1.0')
+plot(log10(((t2r["mcc"]))),(t2r["E"]), label = 'T=2.4')
+title('Disordered (random) initial lattice')
+legend(loc = 'best')
+savefig('4c.pdf')"""
 
+figure(2)
 subplot(2,2,1)
-plot((t1a["mcc"]),t1a["E"])
+t1a_crop = (t1a["E"])[50000::]
+hist(t1a_crop,100,histtype="bar", normed=1, color='green')
 subplot(2,2,2)
-plot(((t1r["mcc"])),(t1r["E"]))
+hist((t1r["E"])[50000::], 100, normed = 1)
 subplot(2,2,3)
-plot(((t2a["mcc"])),(t2a["E"]))
+hist((t2a["E"])[50000::], 100, normed = 1)
 subplot(2,2,4)
-plot(((t2r["mcc"])),(t2r["E"]))
+hist((t2r["E"])[50000::], 100, normed = 1)
 show()
-
-
-
-
-
-
-
-
-
 
 J = 1
 
